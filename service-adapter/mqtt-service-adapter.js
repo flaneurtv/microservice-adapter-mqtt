be9f9973c411f25ce784aa89baa769798659909c
Daniel Avellaneda <daniel.avellaneda@totumo.net>
2017-04-11 15:08:44 -0500

    using subscriptions.txt, uuid, unix time, reading name from package.json

--- service-adapter/mqtt-service-adapter.js
+++ service-adapter/mqtt-service-adapter.js
@@ -1,91 +1,29 @@
 // Setup variables
 var url = require('url');
-var uuid = require('uuid');
+var uuid = require('uuid/v1');
 var mqtt = require('mqtt');
 var readline = require('readline');
 var fs = require('fs');
+var os = require('os');
+var moment = require('moment');
 var child_process = require('child_process');
-var readline = require('readline');
+var pjson = require('../package.json');
+var subscriptions_txt = './service-processor/subscriptions.txt';
 
 // importing all necessary ENV vars
-var namespace = process.env.NAMESPACE || ""
-var service_processor = process.env.SERVICE_PROCESSOR || "./service-processor/processor.sh"
-// TODO: can we read the SERVICE_NAME from package.json?
-// Apparently a json file can be read with 'require'
-var service_name = process.env.SERVICE_NAME || "null"
-var service_uuid = process.env.SERVICE_UUID || uuid() // randomly assigned
-var service_host = process.env.SERVICE_HOST || os.hostname() // equals the docker container ID
+var namespace = process.env.NAMESPACE || "";
+var service_processor = "./service-processor/processor";
 var mqtt_listener_url_object = url.parse(process.env.MQTT_LISTENER_URL || "tcp://mqtt:1883");
 var mqtt_publisher_url_object = url.parse(process.env.MQTT_PUBLISHER_URL || "tcp://mqtt:1883");
-// TODO: I liked the idea of reading this from a textfile. We could add a ./service-processor/subscriptions.txt
-var mqtt_subscriptions = process.env.MQTT_SUBSCRIPTIONS || "";
-
-var subscriptions_txt = './deploy/subscriptions.txt';
-
-// start the processor
-console.log('info: spawning processor: ' + service_processor);
-const processor = child_process.spawn(service_processor, []);
-const processor_stdout = readline.createInterface({ input: processor.stdout});
-const processor_stderr = readline.createInterface({ input: processor.stderr});
-
-// Processor stdout is published on MQTT if connected and valid JSON
-// If MQTT is not connected, lines are dropped to avoid late messages
-processor_stdout.on('line', (line) => {
-  if (JSON.parse(line) && mqtt_publisher.connected == true) {
-    line = line.trim();
-    processor_stdout_message = JSON.parse(line);
-    mqtt_publisher.publish(processor_stdout_message["topic"], line);
-    console.log('processor_stdout_message: ' + line;
-  }
-});
-
-// Processor stderr is forwarded to log line by line
-// QUESTION: I am unsure whether to forward to parent stderr or to stdout via
-// log facility
-processor_stderr.on('line', (line) => {
-  if (line !== null) {
-    line = line.trim();
-    console.log('processor_stderr_message: ' + line;
-  }
-});
-
-// FIXME: define proper log messages to publish on MQTT
-rl.on('close', () => {
-  console.log('event: Readline CLOSE event emitted');
-  if (mqtt_publisher.connected == true) {
-    mqtt_publisher.publish(namespace+'log', '{"service": "status_checker", "event": "readline CLOSE event emitted", "reaction": "terminating"}')
-  }
-  rl.close();
-  mqtt_publisher.end();
-  mqtt_listener.end();
-});
-
-// FIXME: define proper log messages to publish on MQTT
-// FIXME: make sure node exits gracefully on processor termination
-rl.on('SIGINT', () => {
-  console.log('event: Readline SIGINT event emitted');
-  if (mqtt_publisher.connected == true) {
-    mqtt_publisher.publish(namespace+'log', '{"service": "status_checker", "event": "readline SIGINT event emitted", "reaction": "terminating"}')
-  }
-  rl.close();
-  mqtt_publisher.end();
-  mqtt_listener.end();
-});
-
-processor.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
-});
-
+var service_name = pjson.name; // Name of service comes from package.json
+var service_uuid = uuid(); // randomly assigned
+var service_host = os.hostname(); // equals the docker container ID
 
 // Prints MQTT listener/publisher url object
 // console.log('info: mqtt_listener_url parsed:' + JSON.stringify(mqtt_listener_url_object));
 // console.log('info: mqtt_publisher_url parsed:' + JSON.stringify(mqtt_publisher_url_object));
 
 // Connection for MQTT bus listener
-// FIXME: the login credentials shall be read from
-// /run/secrets/mqtt_publisher.json and
-// /run/secrets/mqtt_listener.json
-// If you have a more meaningful name, please tell me.
 var mqtt_listener = mqtt.connect(mqtt_listener_url_object, {
     // username: "type1tv",
     // password: "nuesse",
@@ -119,22 +57,47 @@ mqtt_listener.on("message", function(topic, message) {
     console.log('event => MQTT_MESSAGE_RECEIVED, topic: "' + topic + '", message: "' + message.toString().trim() + '"');
 
     // Forwards message to processor
-    if (processor.connected) {
-      processor.stdin.write(message.toString().trim() + '\n');
-    }
+    // FIXME: the child prosess is not spawned on every message but is a long
+    // living process with an internal loop reading from stdin line by line.
+    // When the process dies, the service-adapter needs to die with it!
+    sp = child_process.spawn(service_processor);
+    sp.stdin.write(message.toString().trim() + '\n');
+
+    // Processor stdout response, publish response
+    // FIXME: I am pretty sure on_data does not suffice here. Because data does
+    // NOT mean line. It is just the current buffer. So it can be half a line or
+    // it can be two lines!!! You have to separate things at the newline symbol.
+    // There is no action until a full line has been received ergo until a
+    // newline has been received. This is why I used readline here!!!
+    sp.stdout.on('data', function (data) {
+        // console.log(data.toString());
+        processor_stdout_message = JSON.parse(data);
+        mqtt_publisher.publish(processor_stdout_message.topic, data.toString());
+    });
+
 });
 
 // Prints when connected to MQTT listener then makes subscriptions
 mqtt_listener.on("connect", (connack) => {
     console.log('event => MQTT listener connected to: "' + mqtt_listener_url_object.href + '"');
-    // Checks subscriptions of ENV
-    if (mqtt_subscriptions !== null) {
-        // Splits subscriptions by ; and then subscribes
-        var arr = mqtt_subscriptions.split(";");
-        for(var i = 0; i < arr.length; i++) {
-            mqtt_listener.subscribe(arr[i]);
-            console.log('event => Topic subscribed: "' + arr[i] + '"');
-        }
+
+    // checks if subscriptions.txt files exists
+    if (fs.existsSync(subscriptions_txt)) {
+        // Reads file subscriptions.txt line by line for MQTT topics the adapter should be subscribed to
+        var rl = readline.createInterface({
+            input: fs.createReadStream(subscriptions_txt)
+        });
+        // With each line of subscriptions.txt create a subscription for the adapter
+        rl.on('line', function(line) {
+            // checks it´s not an empty line
+            if (line !== null && namespace !== "null") {
+                // Subscribe to topic it uses namespace variable, subscriptions.txt file SHOULD NOT have the namespace defined.
+                mqtt_listener.subscribe(namespace + line);
+                console.log("event => Topic subscribed: " + namespace + line);
+            }
+        });
+    } else {
+        console.log("event => subscriptions.txt file is missing, NO TOPICS ARE SUBSCRIBED.");
     }
 });
 
@@ -178,17 +141,36 @@ mqtt_publisher.on("reconnect", function() {
     console.log('event => Trying to reconnect to publisher in: "' + mqtt_publisher_url_object.href + '"');
 });
 
-// Reads file subscriptions.txt line by line for MQTT topics the adapter should be subscribed to
-var rl = readline.createInterface({
-    input: fs.createReadStream(subscriptions_txt)
-});
+// FIXME: this is supposed to be part of the processor not the service-adapter
+// Sends tick test every 3 seconds (3000ms)
+function sendTick() {
+  // FIXME: the "flaneur" part of the topic has to come from the NAMESPACE env
+    mqtt_publisher.publish(namespace + "/tick", JSON.stringify(
+        {
+            "topic": namespace + "/tick",
+            "message": "tick",
+            "created_at": moment().utc().unix(),
+        })
+    );
+}
+setInterval(sendTick, 3000);
 
-// With each line of subscriptions.txt create a subscription for the adapter
-rl.on('line', function(line) {
-    // checks it´s not an empty line
-    if (line !== '') {
-        // Subscribe to topic
-        mqtt_listener.subscribe(line);
-        console.log("event => Topic subscribed: " + line);
-    }
-});
+// FIXME: you could remodle this into the "alive" message functionality I was
+// talking about. This "alive" responder, which responds to the "tick" messages
+// does really belong to the service-adapter code. The tick_responder test case
+// service-processor does not.
+// Sends test after 1 second (1000ms)
+setTimeout(function() {
+    mqtt_publisher.publish(namespace + "/tusd/upload_success", JSON.stringify(
+        {
+            "topic": namespace + "/tusd/upload_success",
+            "service_uuid": service_uuid,
+            "service_name": service_name,
+            "service_host": service_host,
+            "created_at": moment().utc().unix(),
+            "payload": {
+                "tick_uuid": "TICKUUID"
+            }
+        })
+    );
+}, 1000);
